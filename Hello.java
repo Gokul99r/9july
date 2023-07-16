9ad9cbb0d8337199dfb9a44d080c549d0492f13a
Gokul <gokulraj5star@gmail.com>
2023-07-16 15:06:56 +0000

    changes from child

--- Hello.java
+++ Hello.java
@@ -2,11 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-<<<<<<< HEAD
-System.out.println(“hello from local”);
-=======
-System.out.println(“hello from remote”);
->>>>>>> 43d7a9147fff3af920d2fefaefa9ff62b35b75bf
+System.out.println(“hello from child”);
 }
 }
 

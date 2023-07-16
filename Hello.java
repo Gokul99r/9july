0296b10c5054ebb4dd85171c6e5b543dc086f3ce
Gokul <gokulraj5star@gmail.com>
2023-07-16 16:33:51 +0000

    stashed

--- Hello.java
+++ Hello.java
@@ -2,7 +2,11 @@ class Hello
 {
 Public static void main(String args[])
 {
+<<<<<<< Updated upstream
 System.out.println(“hello from merge");
+=======
+System.out.println(“change need to be done in master”);
+>>>>>>> Stashed changes
 }
 }
 

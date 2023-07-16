863318b1b329a4e9f7eb0db0ba958a282b7606bc
Gokul <gokulraj5star@gmail.com>
2023-07-16 16:54:55 +0000

    changes from child to master after committing

--- Hello.java
+++ Hello.java
@@ -2,11 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-<<<<<<< Updated upstream
-System.out.println(“hello from merge");
-=======
-System.out.println(“change need to be done in master”);
->>>>>>> Stashed changes
+system.out.println(“hello from master 3.0”);
 }
 }
 
